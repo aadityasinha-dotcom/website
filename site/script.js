3b3fcf6fc0657b7c85ad5dd32d61cd3d4b8e1903
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-03 21:26:03 +0530

    practise

--- site/script.js
+++
@@ -1,8 +0,0 @@
-function rules() {
-    agree = confirm("RULES: 1. Blah Blah Blah<br> 2. Blah Blah Blah<br> 3. Blah Blah Blah<br> Press OK to agree and Cancel to Dissagree")
-    if (agree == true) {
-      alert("Thank you for agreeing to the rules!")
-    } else {
-      alert("You can not move on unless you agree to the rules.")
-    }
-  }
