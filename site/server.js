ab143e816c423e1fadcf6581d6b9b192eebd37b9
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-06 22:13:54 +0530

    server

--- site/server.js
+++ site/server.js
@@ -3,3 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log('request made');
 });
+
+server.listen(3000, 'localhost',() => {
+    console.log('listening for requests on port 3000')
+})
