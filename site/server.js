c9b261281423729622badb584399fd0eeec81f19
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-07 15:50:08 +0530

    send msg in html type

--- site/server.js
+++ site/server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<p>hello</p>');
+    res.write('<p>hello again</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost',() => {
