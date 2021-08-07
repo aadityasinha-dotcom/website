d63f7952fcaaba605b850e5bad8465c6d7a9c79c
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-07 16:02:52 +0530

    send an html file

--- site/server.js
+++ site/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
@@ -6,10 +7,16 @@ const server = http.createServer((req, res) => {
     //set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<head><link rel="stylesheet" href="#"></head>')
-    res.write('<p>hello</p>');
-    res.write('<p>hello again</p>');
-    res.end();
+    //send an html file
+    fs.readFile('./index.html', (err, data) => {
+        if(err){
+            console.log('error');
+            res.end();
+        }else{
+            res.end(data);
+        }
+    })
+
 });
 
 server.listen(3000, 'localhost',() => {
