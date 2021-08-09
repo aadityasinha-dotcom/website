b97705e486f9de5845079f4cd611f80035164d9c
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-08-09 21:54:03 +0530

    npm libraries

--- site/server.js
+++ site/server.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
             path += 'about.html';
             res.statusCode = 200;
             break;
-        case '/about-me':
+        case '/about-menp':
             res.statusCode = 301;
             res.setHeader('Location', '/about');
             res.end();
